e1c8e544f72bf8531bb57ba12f703a3262df9344
Antonia Elek <antoniaelek@hotmail.com>
2018-12-20 05:00:46 +0100

    Click enabled on win95 card.

--- windows-95/snow.js
+++ windows-95/snow.js
@@ -1,3 +1,15 @@
+function myFunction() {
+  var x = document.getElementById("mydiv");
+  var y = document.getElementById("mydiv2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+	y.style.display = "none";
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+} 
+
 (function() {
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
     function(callback) {
